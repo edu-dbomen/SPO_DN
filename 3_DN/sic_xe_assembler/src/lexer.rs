28f1f4d8db7bf5bbce0adbd4982622b62b379110
dbomen <domen9.breznik@gmail.com>
2025-12-15 22:43:15 +0100

    DN3: added lexer

---
+++ 3_DN/sic_xe_assembler/src/lexer.rs
@@ -0,0 +1,24 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[rustfmt::skip]
+pub fn lexer(file_reader: BufReader<File>) -> Vec<Vec<String>> {
+    let mut res: Vec<Vec<String>> = vec![];
+
+    for line in file_reader.lines() {
+        let line = line.expect("Could not read line");
+
+        res.push(line
+            .split_whitespace()             // split by whitespace
+            .flat_map(|s| s.split(','))     // split by ',' and flatten into single array
+            .map(|s| s.trim())
+            .filter(|s| !s.is_empty())      // filter out empty strings
+            .map(|s| s.to_string())
+            .take_while(|s| s != ".")       // remove comments
+            .collect());
+    }
+
+    res
+}
---
+++ 3_DN/sic_xe_assembler/src/main.rs
@@ -0,0 +1,33 @@
+use std::{env, fs::OpenOptions, io::BufReader, process::exit};
+
+mod lexer;
+// mod parser;
+// mod symbol_resolver;
+// mod code_generator;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Invalid arguments. Use {} <file_name>", args[0]);
+        exit(1);
+    }
+
+    let file_name = &args[1];
+    let file_reader = BufReader::new(
+        OpenOptions::new()
+            .read(true)
+            .open(file_name)
+            .expect("Could not open file"),
+    );
+
+    // .asm lines -> Lexer
+    let lexer_result = lexer::lexer(file_reader);
+
+    // Lexer -> Parser
+
+    // Parser -> Symbol resolver
+
+    // Symbol resolver -> Code generator
+
+    // Code generator -> .obj code
+}
